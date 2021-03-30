43601e31a0c212ef6ac01542e14b05dbae62ee4c
a-velasquez <avelasquez075@gmail.com>
2021-03-29 23:25:19 -0700

    rails g resource Transactions

---
+++ db/migrate/20210330061927_create_transactions.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+class CreateTransactions < ActiveRecord::Migration[6.0]
+  def change
+    create_table :transactions do |t|
+      t.integer :account_id
+      t.float :amount
+      t.string :kind
+      t.datetime :date
+      t.string :description
+
+      t.timestamps
+    end
+  end
+end
