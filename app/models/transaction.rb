0fb46926a84706191bdaee6df089fb3e2e3085d8
a-velasquez <avelasquez075@gmail.com>
2021-03-29 23:32:05 -0700

    add validation that transaction is one of two kinds

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -3,4 +3,5 @@
 class Transaction < ApplicationRecord
   belongs_to :account
   validates :amount, presence: true
+  validates_inclusion_of :kind, in: %w[deposit withdraw] # transaction must be one of two kinds
 end
