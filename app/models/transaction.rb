8be515c6d051d0344af83da7050e36675a11d5c8
a-velasquez <avelasquez075@gmail.com>
2021-03-29 23:57:31 -0700

    add class mthd update_balance

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -2,5 +2,19 @@ class Transaction < ApplicationRecord
   belongs_to :account
   validates :amount, presence: true
   validates_inclusion_of :kind, in: %w[deposit withdraw] # transaction must be one of two kinds
+
+  def update_balance(transaction)
+    if transaction.kind == 'deposit'
+      self.balance = self.balance + transaction.amount
+      self.save
+    elsif transaction.kind == 'withdraw'
+      if self.balance >= transaction.amount 
+        self.balance = self.balance - transaction.amount
+        self.save
+      else
+        return 'Balance too low...'
+      end
+    end
+  end
 end
  
