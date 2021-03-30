950dfc21c1657466d95ce5697d7559cae415592f
a-velasquez <avelasquez075@gmail.com>
2021-03-30 00:13:53 -0700

    add set account with before_action to make code DRY

--- app/controllers/api/v1/transactions_controller.rb
+++ app/controllers/api/v1/transactions_controller.rb
@@ -1,5 +1,7 @@
 class Api::v1::TransactionsController < ApplicationController
 
+  before_action :set_account
+
   def index
 
   end
@@ -17,6 +19,10 @@ class Api::v1::TransactionsController < ApplicationController
   end
 
   private
+
+  def set_account 
+    @account = Account.find(params[:account_id])
+  end
 
   def transaction_params
     params.require(:transaction).permit(:amount, :account_id, :kind, :date, :description)
