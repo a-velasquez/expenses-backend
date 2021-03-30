2379470734c80ef44dfb862f60de1c707b47ddf4
a-velasquez <avelasquez075@gmail.com>
2021-03-29 23:43:13 -0700

    namespace controllers and routes;

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,7 +1,6 @@
-# frozen_string_literal: true
-
 class Transaction < ApplicationRecord
   belongs_to :account
   validates :amount, presence: true
   validates_inclusion_of :kind, in: %w[deposit withdraw] # transaction must be one of two kinds
 end
+ 
